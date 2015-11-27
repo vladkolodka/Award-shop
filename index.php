9dc5815411de46181004038852be83c493c84a99
Vladislav <vladkolodka@gmail.com>
2015-11-27 19:49:51 +0200

    start

---
+++ index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="description" content="Предпокупка игровых привилегий">
+    <meta name="keywords" content="minecraft, awardplay, donate, донат, авардплей, shop, магазин, купить, привилегии">
+
+    <link rel="shortcut icon" type="image/png" href="img/icons/favicon.png">
+    <title>ÀP | shop</title>
+
+    <!-- Styles-->
+    <link rel="stylesheet" type="text/css" href="style/fonts.css">
+    <link rel="stylesheet" type="text/css" href="style/main.css">
+
+    <link rel="stylesheet" type="text/css" href="style/header.css">
+    <link rel="stylesheet" type="text/css" href="style/shop.css">
+    <link rel="stylesheet" type="text/css" href="style/footer.css">
+
+    <!--<link rel="stylesheet" type="text/css" href="style/libs/jquery-ui.css">-->
+
+    <!-- Libs-->
+    <script src="script/libs/jquery.js"></script>
+    <script src="script/libs/jquery-ui.js"></script>
+
+    <!-- My libs-->
+    <script src="script/panoram.js"></script>
+
+    <!-- Scripts -->
+    <script src="script/main.js"></script>
+    <script src="script/shop.js"></script>
+</head>
+<body>
+    <header>
+        <img src="img/header/panoram.jpg" class="panoram">
+    </header>
+</body>
+</html>
---
+++ script/panoram.js
@@ -0,0 +1,7 @@
+var panoram = {
+  init: function(size, scroll){
+      $(".panoram").each(function (index, element) {
+          $(element).css({width: 200});
+      });
+  }  
+};
