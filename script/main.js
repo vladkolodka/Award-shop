bb053006742f17fd7969d1ca946ff09ab04be1ce
Vladislav <vladkolodka@gmail.com>
2015-11-28 20:38:26 +0200

    Main scroller

--- script/main.js
+++ script/main.js
@@ -1,14 +1,36 @@
 $(function () {
-    $(".panorama").panorama(1440, 70000);
+    $(".panorama").panorama(100000);
 
-    // анимация header>h1
-    var header = $("header h1,h3");
-    animateHeader(header);
+    // анимация header>h1,h3
+    var headers = $("header h1,h3");
     window.setTimeout(function () {
-        header.removeClass("scale1");
+        headers.removeClass("scale1");
     }, 200);
-    function animateHeader(header){
+});
 
+/*global Sly, console */
+jQuery(function ($) {
+    'use strict';
 
-    }
+    var options = {
+        scrollBy: 200,
+        speed: 200,
+        easing: 'easeOutQuart',
+        scrollBar: '#scrollbar',
+        dynamicHandle: 1,
+        dragHandle: 1,
+        clickBar: 1,
+        mouseDragging: 1,
+        touchDragging: 1,
+        releaseSwing: 1
+    };
+    var frame = new Sly('#frame', options);
+
+    // Initiate frame
+    frame.init();
+
+    // Reload on resize
+    $(window).on('resize', function () {
+        frame.reload();
+    });
 });
